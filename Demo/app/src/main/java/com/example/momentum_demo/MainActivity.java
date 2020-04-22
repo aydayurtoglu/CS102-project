2e5842f6e3e64071ff4d6137e1c49cd2b80a9c34
Beste-Guney <63497613+Beste-Guney@users.noreply.github.com>
2020-04-22 23:56:50 +0300

    FirstPage
    I tried to make an outline for the first page of the programme

--- Demo/app/src/main/java/com/example/momentum_demo/MainActivity.java
+++ Demo/app/src/main/java/com/example/momentum_demo/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.momentum_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.momentum_demo.Main2Activity;
+import com.example.momentum_demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //properties
+    private Button buttonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = (Button) findViewById(R.id.ButtonStart); //we typecasting to ensure that id belongs to a button
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProjectPage();
+            }
+        });
+    }
+
+    public void openProjectPage()
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
