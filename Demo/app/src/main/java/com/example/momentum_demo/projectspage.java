8e08c49c40b2ec71b1971320df6c3fa5996a9895
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2020-05-03 21:52:31 +0300

    projects page

--- Demo/app/src/main/java/com/example/momentum_demo/projectspage.java
+++ Demo/app/src/main/java/com/example/momentum_demo/projectspage.java
@@ -3,7 +3,6 @@ package com.example.momentum_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
         //private Button add = (Button) findViewById(R.layout.);
@@ -11,6 +10,6 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.projectspage);
     }
 }
