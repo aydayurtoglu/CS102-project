cd735a502f0e4f148d565e934dd5edd0ec202a06
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2020-05-04 20:00:36 +0300

    adjusted projects page

--- Demo/app/src/main/java/com/example/momentum_demo/Main2Activity.java
+++ Demo/app/src/main/java/com/example/momentum_demo/Main2Activity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 
 public class Main2Activity extends AppCompatActivity {
 
     private ImageButton buttonHelp;
     private ImageButton buttonBack;
+    private ImageButton buttonDelete;
+    private ImageButton buttonAdd;
+    private SearchView searchView;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -21,6 +26,18 @@ public class Main2Activity extends AppCompatActivity {
 
         buttonHelp = findViewById(R.id.helpButton);
         buttonBack = findViewById(R.id.backButton);
+        buttonDelete = findViewById(R.id.deleteProjectButton);
+        buttonAdd = findViewById(R.id.addProjectButton);
+
+       // searchView = findViewById(R.id.searchView);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
