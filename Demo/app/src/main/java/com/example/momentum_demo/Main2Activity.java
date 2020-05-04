5917f5582266801d1e9ca90dd2a51644cb1fc885
Ata Seren <ata.seren@ug.bilkent.edu.tr>
2020-05-04 19:09:57 +0300

    some stuff about app

--- Demo/app/src/main/java/com/example/momentum_demo/Main2Activity.java
+++ Demo/app/src/main/java/com/example/momentum_demo/Main2Activity.java
@@ -2,15 +2,18 @@ package com.example.momentum_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private Button buttonHelp;
-    private Button buttonBack;
+    private ImageButton buttonHelp;
+    private ImageButton buttonBack;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
