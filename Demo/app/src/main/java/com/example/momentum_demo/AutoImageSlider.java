3fbf1b8e0964dfdec8875b38b550753781ba7b68
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2020-05-06 19:14:14 +0300

    design

--- Demo/app/src/main/java/com/example/momentum_demo/AutoImageSlider.java
+++ Demo/app/src/main/java/com/example/momentum_demo/AutoImageSlider.java
@@ -2,19 +2,32 @@ package com.example.momentum_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class AutoImageSlider extends AppCompatActivity {
 
     ViewFlipper viewFlipper;
+    Button saveButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_image_slider);
 
         viewFlipper = findViewById(R.id.viewFlipper);
+        saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // videoyu galeriye kaydetsin?
+            }
+        });
 
         int images[] = {R.drawable.logo, R.drawable.logo1}; // BUNLARI Bİ YERDEN ALMAMIZ LAZIM
 
