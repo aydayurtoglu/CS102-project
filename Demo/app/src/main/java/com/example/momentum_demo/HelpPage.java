d5ccfb469b0f99723bfced038ba764df83696d14
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2020-05-06 15:51:44 +0300

    design

--- Demo/app/src/main/java/com/example/momentum_demo/HelpPage.java
+++ Demo/app/src/main/java/com/example/momentum_demo/HelpPage.java
@@ -9,21 +9,10 @@ import android.widget.ImageButton;
 
 public class HelpPage extends AppCompatActivity {
 
-    private ImageButton buttonBack;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
 
-        buttonBack = findViewById(R.id.backButton);
-
-        buttonBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HelpPage.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
