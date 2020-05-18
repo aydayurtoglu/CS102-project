5ae38b872c39bf92a341e011e0013a3892433fdb
Ayda Yurtoglu <aydayurtoglu@gmail.com>
2020-05-18 21:47:46 +0300

    arrays

--- Demo/app/src/main/java/com/example/momentum_demo/ImageAdapter.java
+++ Demo/app/src/main/java/com/example/momentum_demo/ImageAdapter.java
@@ -16,7 +16,12 @@ import android.widget.ImageView;
 public class ImageAdapter<imageArray> extends BaseAdapter {
 
     private Context mContext;
-    public int[] imageArray = {}; // buraya çektiğimiz resimleri koymamız lazım
+    public int[] imageArray = {R.drawable.cat1, R.drawable.cat2, R.drawable.cat3, R.drawable.cat4,
+            R.drawable.cat5, R.drawable.cat6, R.drawable.cat7, R.drawable.cat8, R.drawable.cat9,
+            R.drawable.cat10, R.drawable.cat11, R.drawable.cat12, R.drawable.cat13, R.drawable.cat14,
+            R.drawable.cat15, R.drawable.cat16, R.drawable.cat17, R.drawable.cat18, R.drawable.cat19,
+            R.drawable.cat20, R.drawable.cat21, R.drawable.cat22, R.drawable.cat23, R.drawable.cat24,
+            R.drawable.cat25, R.drawable.cat26, R.drawable.cat27, R.drawable.cat28}; // buraya çektiğimiz resimleri koymamız lazım
 
     //int rh= this.getResources().getIdentifier("ic_launcher", "drawable", this.getPackageName());
 
